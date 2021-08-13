78f75e904db23639039d4c2ddc05878d07baf008
ithemia <ithemia@itcast.cn>
2021-08-13 22:41:54 +0800

    完成登录和注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,66 +1,54 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
-  })
+$(function () {
+  $("#reg_box").on("click", function () {
 
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
-  })
+    $(".login-box").show()
+    $(".reg-box").hide()
 
-  // 从 layui 中获取 form 对象
+  })
+  $("#login_box").on("click", function () {
+    $(".reg-box").show()
+    $(".login-box").hide()
+
+  })
   var form = layui.form
   var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
   form.verify({
-    // 自定义了一个叫做 pwd 校验规则
+    // 自定义校验规则
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
+    repwd: function (value) {
+      var pwd = $('.reg-box [name=password]').val();
       if (pwd !== value) {
-        return '两次密码不一致！'
+        return '两次密码不一致！';
       }
-    }
-  })
-
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
+    },
+  });
+  $("#regbox").on("submit", function (e) {
     e.preventDefault()
-    // 2. 发起Ajax的POST请求
     var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
+      username: $('.reg-box [name="username"]').val(),
+      password: $('.reg-box [name="password"]').val(),
+      repassword: $('.reg-box [name="repassword"]').val()
     }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
+
+    // console.log(data);
+    $.post("/api/reg", data, function (res) {
+      console.log(res);
+      if (res.code !== 0) {
         return layer.msg(res.message)
       }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
+      layer.msg("注册登录成功")
+      $("#reg_box").click()
     })
   })
-
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
+  $("#loginbox").submit(function (e) {
+    console.log(1111111);
     e.preventDefault()
     $.ajax({
-      url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
+      url: "/api/login",
+      method: "POST",
       data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
+      success: function (res) {
+        if (res.code !== 0) {
           return layer.msg('登录失败！')
         }
         layer.msg('登录成功！')
